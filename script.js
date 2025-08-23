60aebe6921cdd9b14f68b8bd55c1066ac1edeb0b
masi222 <mm7166549@gmail.com>
2025-08-23 18:57:15 -0400

    Implementada sección de freelancers con tarjetas interactivas y navegación suave mediante JavaScript.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Archivo JavaScript inicial 
+// Navegación suave para enlaces de navegación
+document.querySelectorAll('a[href^="#"]').forEach(enlace => {
+    enlace.addEventListener('click', function (evento) {
+        evento.preventDefault();
+        const objetivo = document.querySelector(this.getAttribute('href'));
+        if (objetivo) {
+            objetivo.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
